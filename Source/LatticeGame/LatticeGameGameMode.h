f2c037ba7c0b7c8a1c1e5cd811f2c9a030b08e9f
Briana O'Hern <brianaohern@gmail.com>
2017-07-15 16:40:57 -0700

    Initial commit

---
+++ Source/LatticeGame/LatticeGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LatticeGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALatticeGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALatticeGameGameMode();
+};
+
+
+
